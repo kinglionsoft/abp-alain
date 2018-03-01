4547c5252114199d64396ee23252f5a5cf0f3e11
yangchao <yangchao@yun-kai.com>
2018-03-01 17:47:19 +0800

    添加abp组件

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,11 @@
+///<reference path="../node_modules/abp-web-resources/Abp/Framework/scripts/abp.d.ts"/>
+///<reference path="../node_modules/abp-web-resources/Abp/Framework/scripts/libs/abp.jquery.d.ts"/>
+///<reference path="../node_modules/abp-web-resources/Abp/Framework/scripts/libs/abp.signalr.d.ts"/>
+///<reference path="../node_modules/moment/moment.d.ts"/>
+///<reference path="../node_modules/@types/moment-timezone/index.d.ts"/>
+///<reference path="../node_modules/@types/jquery/index.d.ts"/>
+///<reference path="../node_modules/@types/signalr/index.d.ts"/>
+
 /* SystemJS module definition */
 declare var module: NodeModule;
 interface NodeModule {
